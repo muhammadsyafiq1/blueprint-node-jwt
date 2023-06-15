9a9a32fd2d130d71f63e1e938ec23fd8c19abdec
muhammadsyafiq1 <72643465+muhammadsyafiq1@users.noreply.github.com>
2023-06-15 16:01:00 +0700

    Update VerifyToken.js

--- middleware/VerifyToken.js
+++ middleware/VerifyToken.js
@@ -1,18 +1,22 @@
-import jwt from "jsonwebtoken"
-
 export const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    // memggunakan split karena bearer dan token ada space dan ambil index 1 untuk ambil token
-    // operator && apabila user tidak mengirim token maka variabel tokennya juga kosong
-    const token = authHeader && authHeader.split(' ')[1];
-    if(token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
-        if(err) return res.sendStatus(403);
-        req.email = decoded.email;
-        next();
-    })
-}
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
 
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403);
 
+    const { email } = decoded;
 
+    // Dapatkan nilai role_id dari objek req
+    const { role_id } = req;
 
+    // Lakukan verifikasi berdasarkan role_id
+    if (role_id !== 1) {
+      return res.sendStatus(403);
+    }
+
+    req.email = email;
+    next();
+  });
+};
