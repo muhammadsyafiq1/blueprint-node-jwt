b0ce12a020a3d8877f8958bb8e9379583c2c8518
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2023-08-11 18:39:09 +0700

    update

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -23,32 +23,32 @@ export const getProductById = async(req, res) => {
 }
 
 export const saveProduct = async (req, res) => {
-    console.log();
+
     if(req.files === null) return res.status(400).json({msg: "No File Uploaded"});
     const name = req.body.name
-    const gambar = req.files.gambar
-    const gambarSize = gambar.data.length
-    const ext = path.extname(gambar.name)
-    const gambarName = gambar.md5 + ext
-    const url = `${req.protocol}://${req.get("host")}/images/${gambarName}`
+    const file = req.files.file
+    const fileSize = file.data.length
+    const ext = path.extname(file.name)
+    const fileName = file.md5 + ext
+    const url = `${req.protocol}://${req.get("host")}/images/${fileName}`
     const allowedType = ['.png','.jpg','.jpeg']
 
     if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "invalid ext"})
-    if(gambarSize > 5000000) return res.status(422).json({msg: "image must be less than 5 mb"})
+    if(fileSize > 5000000) return res.status(422).json({msg: "image must be less than 5 mb"})
     //Simpan image dalam folder image
-gambar.mv(`./public/images/${gambarName}`, async(err) => {
-    if(err) return res.status(500).json({msg: err.message})
-    try {
-        await Product.create({
-            name: name,
-            image: gambarName,
-            url: url
-        })
-        res.status(201).json({msg: "product has been created"})
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+    file.mv(`./public/images/${fileName}`, async(err) => {
+        if(err) return res.status(500).json({msg: err.message})
+        try {
+            await Product.create({
+                name: name,
+                image: fileName,
+                url: url
+            })
+            res.status(201).json({msg: "product has been created"})
+        } catch (error) {
+            console.log(error.message);
+        }
+    })
 
 }
 
